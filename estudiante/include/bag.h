dfe9782753e5f9a640d724ec9a33a9eb7195c507
Adrián Jaén Fuentes <76180710+Mayhem929@users.noreply.github.com>
2021-12-04 20:26:15 +0100

    bag.h y bag.cpp
    falta documentar bag.h y revisar algunos metodos de bag.cpp

--- estudiante/include/bag.h
+++ estudiante/include/bag.h
@@ -1,5 +1,12 @@
+//
+// Created by Adrián Jaén on 04/12/2021.
+//
+
 #ifndef __BAG_H__
 #define __BAG_H__
+
+#include "vector"
+using namespace std;
 
 /**
  *  \brief TDA abstracto Bolsa
@@ -8,4 +15,60 @@
  *  permite la extracción de elementos de forma aleatoria sin reemplazamiento
  */
 
-#endif
+template<class T>
+class Bag {
+
+private:
+    vector<T> v;
+public:
+
+    /**
+     * @brief Base constructor
+     */
+    Bag();
+
+    /**
+     * @brief Constructor de copia. Crea una copia exacta de otro objeto tipo Bag
+     * @param other Objeto de tipo Bag<T> del que se va a realizar la copia
+     */
+    Bag(const Bag<T> &other);
+
+    /**
+     *
+     * @param element
+     */
+    void add(const T &element);
+
+    /**
+     *
+     * @return
+     */
+    T get();
+
+    /**
+     *
+     */
+    void clear();
+
+    /**
+     *
+     * @return
+     */
+    unsigned int size();
+
+    /**
+     *
+     * @return
+     */
+    bool empty();
+
+    /**
+     *
+     * @param other
+     * @return
+     */
+    const Bag<T>& operator=(const Bag<T> &other);
+
+};
+
+#endif //__BAG_H__
---
+++ estudiante/src/bag.cpp
@@ -0,0 +1,57 @@
+//
+// Created by Adrián Jaén on 04/12/2021.
+//
+
+#include "bag.h"
+#include <random>
+
+template<class T>
+Bag<T>::Bag(){
+    this->v=vector<T>{};
+}
+
+template<class T>
+Bag<T>::Bag(const Bag<T> &other) {
+    this->v = other.v;
+}
+
+template<class T>
+void Bag<T>::add(const T &element) {
+    if (v.size() == 0)
+        v.push_back(element);
+    else {
+        int randNum = rand()%v.size();
+        v.push_back(v[randNum]);
+        v[randNum] = element;
+    }
+}
+
+template<class T>
+T Bag<T>::get() {
+    int randNum = rand()%v.size();
+    T element = v[randNum];
+    v[randNum] = v[v.size()-1];
+    v.pop_back();
+    return element;
+}
+
+template<class T>
+void Bag<T>::clear() {
+    v.clear();
+}
+
+template<class T>
+unsigned int Bag<T>::size() {
+    return v.size();
+}
+
+template<class T>
+bool Bag<T>::empty() {
+    return v.empty();
+}
+
+template<class T>
+const Bag<T> &Bag<T>::operator=(const Bag<T> &other) {
+    *this->v = other.v;
+    return *this;
+}
